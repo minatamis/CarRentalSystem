2a4ed16ef65da551a12299740488c53c6b02d2f3
NelsonAbn <jamesabuan147@gmail.com>
2023-07-20 15:52:54 +0800

    Adding Customer database

---
+++ CustomerInfo.sql
@@ -0,0 +1,53 @@
+/*CREATE DATABASE dbCustomerInfo;*/
+
+/*CREATE TABLE tblCustomerInformation(
+Customerusername varchar(25) not null,
+CustomerPassword varchar(25) not null,
+UID int primary key auto_increment,
+UserName varchar(25) not null,
+Age int not null,
+Email varchar(25) not null,
+ContactNumber varchar(11),tblcustomerinformation
+Address varchar(200)
+);*/
+
+/*SELECT * FROM tblCustomerInformation;*/
+
+/*INSERT INTO tblCustomerInformation(
+CustomerUserName,
+CustomerPassword,
+UserName,
+Age,
+Email,
+ContactNumber,
+Address
+) VALUES 
+("Nelsonabn","Sigeokaylang","Nelson James Abuan",20,"jamesabuan147@gmail.com","09509440025","Timbao Binan Laguna"),
+("Jemcbn","pagodnako","Jem Cebuano",20,"jemcebuano333@gmail.com","09192363772","Sto Tomas Binan Laguna"),
+("Euniceems","MLislife","Eunice Stephanie Emata",20,"Emata513@gmail.com","09534472216","Sto Nino Binan Laguna"),
+("iamvicss","pullup","Victor troy Avila",20,"Vicss1432@gmail.com","09246336091","Brgy Binan Binan Laguna"),
+("Jmtalksalot","movealong","JM Dinglasan",20,"Dinglasan12342","09284466841","Carmona Cavite")*/
+
+
+/*ALTER TABLE tblCustomerInformation
+RENAME COLUMN Age TO UserAge;
+
+ALTER TABLE tblCustomerInformation
+RENAME COLUMN Email TO UserEmail;
+
+ALTER TABLE tblCustomerInformation
+RENAME COLUMN ContactNumber TO UserNumber;
+
+ALTER TABLE tblCustomerInformation
+RENAME COLUMN Address TO UserAddress;*/
+
+
+
+
+
+
+
+
+
+
+
