bfd2593820e4276bb062d93a0209fe21e757137e
NelsonAbn <jamesabuan147@gmail.com>
2023-07-21 09:35:37 +0800

    Inserting Database

---
+++ userinfo.sql
@@ -0,0 +1,5 @@
+INSERT INTO `userinfo` VALUES ('Eunice Emata','MLislife',20,'Emata513@gmail.com','09534472216','Sto Nino Binan Laguna',''),('Jem Cebuano','pagodnako',21,'jemcebuano333@gmail.com','09192363772','Sto Tomas Binan Laguna',''),('jm','jm1233',21,'jm@gmail.com','09217299080','carmona',''),('John Mari Dinglasan','movealong',20,'Dinglasan12342','09284466841','Carmona Cavite',''),('Nelson Abuan','Sigeokaylang',20,'jamesabuan147@gmail.com','09509440025','Timbao Binan Laguna',''),('Victor Troy Avila','pullup',20,'Vicss1432@gmail.com','09246336091','Brgy Binan Binan Laguna',''),('yunis','yunis1233',20,'yunis@gmail.com','09999974478','binan','');
+
+
+
+
