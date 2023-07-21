522433f1ff8efd2780b199e5f22218fcb5f25910
Jem <116412303+minatamis@users.noreply.github.com>
2023-07-21 20:30:50 +0800

    Update Confirmation.java

--- src/main/java/com/mycompany/carrentalsystem/Confirmation.java
+++ src/main/java/com/mycompany/carrentalsystem/Confirmation.java
@@ -2,6 +2,11 @@ package com.mycompany.carrentalsystem;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.swing.*;
 
 public class Confirmation extends JFrame implements ActionListener
@@ -10,6 +15,8 @@ public class Confirmation extends JFrame implements ActionListener
     JTextField clientUsernameTextField,clientCarChoiceTxtField;
     JPasswordField clientPasswordTextField;
     JButton rentButton, btnNewButton;
+    private Connection connection;
+    String car, username, password;
     Confirmation()
     {
         setTitle("Confirm your Rent");
@@ -61,7 +68,71 @@ public class Confirmation extends JFrame implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        username = clientUsernameTextField.getText();
+        password = clientPasswordTextField.getText();
+        car = clientCarChoiceTxtField.getText();
         
+        try
+        {
+            connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/carrentalsystem","root", "Jem4764?");
+            connection.setAutoCommit(false);
+//            PreparedStatement userPrepStatement = (PreparedStatement) connection.prepareStatement(userSt);
+//            PreparedStatement carPrepStatement = (PreparedStatement) connection.prepareStatement(carSt);
+            if(updateUserTable(connection, username, password, car))
+            {
+                updateCarTable(connection,car);
+            }            
+           
+            
+
+        }catch (SQLException sqlException) 
+        {
+            sqlException.printStackTrace();
+        }
+
+    }
+    public boolean updateUserTable(Connection connection, String email, String pass, String car) throws SQLException 
+    {
+        String userSt = "UPDATE userinfo SET carInRent=? WHERE userEmail=? and userPassword=?";
+        try (PreparedStatement userPrepStatement = connection.prepareStatement(userSt)) 
+        {
+            userPrepStatement.setString(1, car);
+            userPrepStatement.setString(2, email);
+            userPrepStatement.setString(3, pass);
+
+            int rowsAffected = userPrepStatement.executeUpdate();
+            if(rowsAffected > 0)
+            {
+                dispose();
+                JOptionPane.showMessageDialog(btnNewButton, "You've been Registered Successfully");
+                return true;
+            }
+            return false;
+        }
+
+    }
+    
+    public void updateCarTable(Connection connection, String model) throws SQLException 
+    {
+        String carSt = "UPDATE car SET Availability=? WHERE Model=? and Availability=?";
+        try (PreparedStatement carPrepStatement = connection.prepareStatement(carSt)) 
+        {
+            carPrepStatement.setString(1, "Unavailable");
+            carPrepStatement.setString(2, model);
+            carPrepStatement.setString(3, "Available");
+
+            int rowsAffected = carPrepStatement.executeUpdate();
+            if(rowsAffected > 0)
+            {
+                dispose();
+                JOptionPane.showMessageDialog(btnNewButton, "You've Rented Successfully");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(btnNewButton, "The Car is Unavailable");
+            }
+        }
+
     }
 
 }
